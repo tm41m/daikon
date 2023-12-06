8e54c82e362d4fafd6ebe1c0d20350422afc6cd0
Andrew Shannon <a.shannon9@gmail.com>
2023-12-06 13:56:57 -0500

    test for census division search endpoint

--- daikon/test/e2e/test_api_endpoint.py
+++ daikon/test/e2e/test_api_endpoint.py
@@ -10,7 +10,7 @@ def test_api_product_metrics_search_endpoint():
     response = requests.get(url, params=params)
 
     data = response.json()
-    
+
     expected_data = [
         {
             "avg_price": "6.09",
@@ -37,14 +37,35 @@ def test_api_product_metrics_search_endpoint():
             "product_listings_rtn": 98,
             "region_code": "ON",
             "unit": "/ 1kg",
-        }
+        },
     ]
 
     assert expected_data == data
 
-    params2 = {"start_date": "2023-01-01", "end_date": "2023-01-02", "product_id": 1, "census_division_name": "Wellington"}
+    params2 = {
+        "start_date": "2023-01-01",
+        "end_date": "2023-01-02",
+        "product_id": 1,
+        "census_division_name": "Wellington",
+    }
     response2 = requests.get(url, params=params2)
 
     data2 = response2.json()
 
-    assert data2 == {"error":"census_division_name cannot be passed without region_code, refer to https://tm41m.io/docs/daikon/product_timeseries_metrics.html"}
+    assert data2 == {
+        "error": "census_division_name cannot be passed without region_code, refer to https://tm41m.io/docs/daikon/product_timeseries_metrics.html"
+    }
+
+
+def test_api_census_divisions_search_endpoint():
+    url = DAIKON_HOST_URL + "/census-divisions/search"
+    params = {"name": "Kawartha Lakes"}
+    response_name_only = requests.get(url, params=params)
+
+    data_name_only = response_name_only.json()
+
+    expected_data_name_only = {
+        "error": "Invalid query params, refer to https://tm41m.io/docs/daikon/census_divisions.html"
+    }
+
+    assert data_name_only == expected_data_name_only
